58bbfb496938dd64777bef1aa16821c2d40339e9
avmugesh <46862942+avmugesh@users.noreply.github.com>
2025-03-29 02:15:51 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "bucket_name" {
+  description = "S3 bucket name"
+  type        = string
+}
+
+variable "environment" {
+  description = "Deployment environment (e.g., dev, staging, prod)"
+  type        = string
+  default     = "dev"
+}
